eb4a93e933c45c5c27c214af0ba3878557f0d54c
bdntplay <peppersbre@yahoo.com>
2021-12-05 20:01:39 -0500

    initial setup

---
+++ src/components/webapp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+class WebApp extends React.Component{
+    render() {
+        return (
+             <div className='App'>CMA4</div>
+        );
+    }
+}
+
+
+export default WebApp;
