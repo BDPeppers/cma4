9b8dc9f0afd45b0d3213be3abde48071063ea339
bdntplay <peppersbre@yahoo.com>
2022-04-19 23:41:31 -0400

    style cleanup

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9097601d9072b5c3"; }
+/******/ 		__webpack_require__.h = function() { return "af6c0b1c59d15f8a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
